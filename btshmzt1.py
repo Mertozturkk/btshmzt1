dedc79997cf69353a4fb13f9f6b14f3a3b544cb0
Mert Öztürk <42963332+Mertozturkk@users.noreply.github.com>
2021-09-06 23:37:29 +0300

    Update and rename version1.py to btshmzt1.py

--- btshmzt1.py
+++ btshmzt1.py
@@ -3,20 +3,25 @@ from bs4 import BeautifulSoup as bs
 import hashlib
 import pandas as pd
 import numpy as np
+import urllib.request
 
 site = "http://www.sahibinden.com/kategori-vitrin?viewType=List&category=3530"
 headers = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1 (.NET CLR 3.5.30729)", "Referer": "http://example.com"}
 # Sitenin negelleyebileceği bot vb. durumlar ve hata almamak için header oluşturduk
+
 ITEM_URL_ = []
 ITEM_URL_SHA1 = []
+pictures = []
 
 URL_SHA1 = hashlib.sha1(site.encode())
 
 
+r = requests.get(site,headers = headers)   #İlgili sayfadan request isteğimizi alıyoruz
+soup = bs(r.content,'html.parser')
+links = soup.find("tbody", class_="searchResultsRowList") 
+
 def scraper():
-    r = requests.get(site,headers = headers)   #İlgili sayfadan request isteğimizi alıyoruz
-    soup = bs(r.content,'html.parser')
-    links = soup.find("tbody", class_="searchResultsRowList") # linklere ulaşabilmek için tbody ve ilgili classa ulaştık
+    # linklere ulaşabilmek için tbody ve ilgili classa ulaştık
     
     for a in links.find_all('a', href=True): #linkleri bir listeye topladık
         ITEM_URL_.append(a['href'])
@@ -37,10 +42,20 @@ def scraper():
     ITEM_URL_SHA2.to_csv('ITEM_URL_SHA1.csv')
 
 
+def get_pic():
+    
+    counter_ = 1
+    for a in links.find_all('img', src=True): #linkleri bir listeye topladık
+        pictures.append(a['src'])
 
+    for url in pictures:
+        urllib.request.urlretrieve(url,'Resim'+ str(counter_)+'.jpg')
+        counter_ +=1
 
 
 
 if __name__== "__main__":
     scraper()
+    get_pic()
+  
 
